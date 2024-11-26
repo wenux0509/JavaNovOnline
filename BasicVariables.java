1d9e99c240546bec25ae236843bf1a830ef5153a
wenux0509 <wenux1234@gmail.com>
2024-11-26 15:17:57 +0800

    This is the first commit of the Java Novemeber class

---
+++ BasicVariables.java
@@ -0,0 +1,6 @@
+public class BasicVariables {
+    public static void main(String[] args) {
+        String name ="Hello Java Class";
+        System.out.println(name);
+    }
+}
---
+++ StaticFile.java
@@ -0,0 +1,9 @@
+public class StaticFile {
+    private String name;
+    private String path;
+    private String type;
+    public static void main(String[]arg){
+
+    }
+}
+
