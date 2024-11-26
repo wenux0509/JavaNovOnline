93bd4ea1a08b311c9a596a8083c807ede71fcf57
wenux0509 <wenux1234@gmail.com>
2024-11-26 16:34:19 +0800

    The first code

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    public static void main (String[]args){
+        String name = "Tan";
+        int age = 20;
+        double height = 1.75;
+        boolean isStudent = true;
+        long networth = 2500000000000000000L;
+        System.out.println("The student name is "+ name);
+        System.out.println("The student age is "+ age);
+        System.out.println("The student height is "+ height);
+        System.out.println("The student networth is RM "+ networth);
+    }
+}
